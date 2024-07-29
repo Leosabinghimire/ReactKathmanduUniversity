b152b5773bb7bc4f546b2a55f226aec6e550a7db
leosabinghimire <sabinghimire2055@gmail.com>
2024-07-29 12:15:41 +0545

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 export const App = () => {
   return (
@@ -12,6 +14,10 @@ export const App = () => {
       <div className="container">
         <Title subTitle="OUR PROGRAM" title="What We Offer" />
         <Programs />
+        <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
       </div>
     </div>
   );
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./About.css";
+import about_img from "../../assets/about.png";
+import play_icon from "../../assets/play-icon.png";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left">
+        <img src={about_img} alt="" className="about-img" />
+        <img src={play_icon} alt="" className="play-icon" />
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies quam
+          amet himenaeos, ornare tempor elementum? Fringilla rhoncus per egestas
+          urna montes viverra viverra cras sollicitudin. Elementum taciti duis
+          lacus fringilla semper ultrices class. Netus porta eu euismod.
+        </p>
+        <p>
+          ullamcorper suspendisse. Vel integer pellentesque nam conubia
+          curabitur porta habitant praesent. Commodo rutrum dolor id hendrerit
+          volutpat nisi. Gravida euismod aliquam blandit sapien dignissim proin;
+          aptent dis. Nascetur ornare penatibus magna lectus dolor parturient
+          accumsan.
+        </p>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Ultricies quam
+          amet himenaeos, ornare tempor elementum? Fringilla rhoncus per egestas
+          urna montes viverra viverra cras sollicitudin. Elementum taciti duis
+          lacus fringilla semper ultrices class. Netus porta eu euismod.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
